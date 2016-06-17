45fc64a1af4db8ce4f3dcae2c894833b203d8a74
Bran van der Meer <branmovic@gmail.com>
2016-06-17 09:30:37 +0200

    Refactor: JSON Schema is now used for config validation

--- app/cli/userConfigLoader.js
+++ app/cli/userConfigLoader.js
@@ -1,5 +1,6 @@
 var argLoader = require('./argumentLoader');
 var util      = require('../util');
+var ajv       = require('ajv')({ allErrors: true });
 
 /**
  * Expose module functions
@@ -22,154 +23,26 @@ function getUserConfig() {
     var config = argLoader.getConfig();
 
     if (!util.fileExists(config.config)) {
-        throw new Error('Config file not found: ' + config.config);
+        throw new Error('\n Config file not found: ' + config.config);
     }
 
     try {
         var userConfig = require(config.config);
     } catch (e) {
-        throw new Error(util.format("Could not load config file '%s' (Incorrect JSON?): ", config.config));
+        throw new Error(util.format("\n Could not load config file '%s' (Incorrect JSON?): ", config.config));
     }
 
-    // This function throws on error
-    validateUserConfig(userConfig);
-
-    // Slugify page and component identifiers
-    userConfig = slugifyUserConfig(userConfig);
-
-    return userConfig;
-
-}
-
-/**
- * Validate user config into app config object
- *
- * @private
- * @param {UserConfig} userConfig
- * @returns {Boolean}
- */
-function validateUserConfig(userConfig) {
-
-    // Validate phantomjs flags config
-    if (typeof userConfig['phantomjs-flags'] !== 'undefined') {
-        if (!Array.isArray(userConfig['phantomjs-flags'])) {
-            throw new Error('Config: phantomjs-flags must be an array of strings!');
-        }
-        userConfig['phantomjs-flags'].forEach(flag => {
-            if (typeof flag !== 'string') {
-                throw new Error('Config: phantomjs-flags must be an array of strings!');
-            }
-        });
-    }
-
-    // Validate credentials
-    if (typeof userConfig.credentials !== 'undefined') {
-        if (typeof userConfig.credentials !== 'string' || userConfig.credentials.length < 3) {
-            throw new Error('Config: credentials must be a string!');
-        }
-        var credentials = userConfig.credentials.split(':');
-        if (credentials.length !== 2) {
-            throw new Error('Config: credentials must be a correctly formatted string!');
-        }
-    }
-
-    // Validate sizes
-    if (!Array.isArray(userConfig.sizes) || userConfig.sizes.length < 1) {
-        throw new Error('Config: No sizes found!');
-    }
-    userConfig.sizes.forEach(size => {
-        if (typeof size !== 'string') {
-            throw new Error('Config: All sizes need to be a string!');
-        }
-        var sizes = size.split('x').map(x => parseInt(x, 10));
-        if (sizes.length !== 2 || !(sizes[0] > 0) || !(sizes[1] > 0)) {
-            throw new Error('Config: All sizes need to be a correctly formatted string!');
-        }
-    });
-
-    // Validate pages
-    if (!Array.isArray(userConfig.pages) || userConfig.pages.length < 1) {
-        throw new Error('Config: No pages found!');
-    }
-    userConfig.pages.forEach(page => {
-        if (!page.name || typeof page.name !== 'string' || !page.url || typeof page.url !== 'string') {
-            throw new Error('Config: All pages need a name and url!');
-        }
-        if (!Array.isArray(page.components) || !page.components.length) {
-            throw new Error(util.format("Config: Page '%s' needs at least 1 component!", page.name));
-        }
-    });
-
-    // Validate components
-    if (!Array.isArray(userConfig.components) || userConfig.components.length < 1) {
-        throw new Error('Config: No components found!');
-    }
-    userConfig.components.forEach(component => {
-        if (!component.name || typeof component.name !== 'string' || !component.selector || typeof component.selector !== 'string') {
-            throw new Error('Config: All components need a name and selector!');
-        }
-        if (typeof component.ignore !== 'undefined' && (!Array.isArray(component.ignore) || !component.ignore.length)) {
-            throw new Error(util.format("Config: Component '%s' has an invalid ignore list!", component.name));
-        }
-    });
-
-    // Validate all referenced components actually exist
-    var components = userConfig.components.map(c => c.name);
-    userConfig.pages.forEach(page => {
-        page.components.forEach(component => {
-            if (!~components.indexOf(component)) {
-                throw new Error(util.format("Config: Component '%s' not found in page '%s'", component, page.name));
-            }
-        });
-    });
-
-    // Validate all wait-for-script levels
-    if (typeof userConfig['wait-for-script'] !== 'undefined' && typeof userConfig['wait-for-script'] !== 'string') {
-        throw new Error('Config: wait-for-script must be a valid JavaScript string!');
-    }
-    userConfig.pages.forEach(page => {
-        if (typeof page['wait-for-script'] !== 'undefined' && typeof page['wait-for-script'] !== 'string') {
-            throw new Error(util.format("Config: Page '%s' has an invalid wait-for-script!", page.name));
-        }
-    });
-    userConfig.components.forEach(component => {
-        if (typeof component['wait-for-script'] !== 'undefined' && typeof component['wait-for-script'] !== 'string') {
-            throw new Error(util.format("Config: Component '%s' has an invalid wait-for-script!", component.name));
-        }
-    });
-
-    // Validate all wait-for-delay levels
-    if (typeof userConfig['wait-for-delay'] !== 'undefined' && typeof userConfig['wait-for-delay'] !== 'number') {
-        throw new Error('Config: wait-for-delay must be a valid JavaScript number!');
-    }
-    userConfig.pages.forEach(page => {
-        if (typeof page['wait-for-delay'] !== 'undefined' && typeof page['wait-for-delay'] !== 'number') {
-            throw new Error(util.format("Config: Page '%s' has an invalid wait-for-delay!", page.name));
-        }
-    });
-    userConfig.components.forEach(component => {
-        if (typeof component['wait-for-delay'] !== 'undefined' && typeof component['wait-for-delay'] !== 'number') {
-            throw new Error(util.format("Config: Component '%s' has an invalid wait-for-delay!", component.name));
-        }
-    });
-
-    // Throw 'removed' errors for finished-when
-    var msg = 'Config: finished-when was renamed to wait-for-script in v0.6.0, please update your config';
-    if (typeof userConfig['finished-when'] !== 'undefined') {
+    const schema = require('../../user-config.schema.json');
+    const valid = ajv.validate(schema, userConfig);
+    if (!valid) {
+        const msg = '\n ' + ajv.errors
+            .map(err => (err.dataPath ? '`' + err.dataPath + '` ' : 'root ') + err.message)
+            .join('\n ');
         throw new Error(msg);
     }
-    userConfig.pages.forEach(page => {
-        if (typeof page['finished-when'] !== 'undefined') {
-            throw new Error(msg);
-        }
-    });
-    userConfig.components.forEach(component => {
-        if (typeof component['finished-when'] !== 'undefined') {
-            throw new Error(msg);
-        }
-    });
 
-    return true;
+    // Slugify page and component identifiers
+    return slugifyUserConfig(userConfig);
 
 }
 
--- tests/spec/configtest.js
+++ tests/spec/configtest.js
@@ -49,163 +49,35 @@ describe('Action: Configtest', function() {
 
     });
 
-    it('should fail without sizes', function() {
+    it('should fail with and incorrect config file', function() {
 
-        var args = [
-            normalize(__dirname + '/../../bin/argus-eyes.js'),
-            'configtest',
-            '--config=tests/fixtures/configtest/missing-sizes.json'
+        var tests = [
+            ['missing-sizes.json', /sizes/],
+            ['invalid-sizes.json', /sizes/],
+            ['missing-pages.json', /pages/],
+            ['invalid-pages.json', /pages/],
+            ['missing-components.json', /components/],
+            ['invalid-components.json', /components/],
+            ['invalid-wait-for-script.json', /wait-for-script/],
+            ['invalid-wait-for-delay.json', /wait-for-delay/],
+            ['invalid-phantomjs-flags.json', /phantomjs-flags/]
         ];
 
-        var proc = spawnSync('node', args, { encoding: 'utf8' });
+        tests.forEach(function(test) {
 
-        assert.equal(proc.status, 1, 'Exitcode not 1!');
-        assert.equal(/Config invalid/.test(proc.stdout), true, proc.stdout);
-        assert.equal(/sizes/.test(proc.stdout), true, proc.stdout);
+            var args = [
+                normalize(__dirname + '/../../bin/argus-eyes.js'),
+                'configtest',
+                '--config=tests/fixtures/configtest/' + test[0]
+            ];
 
-    });
+            var proc = spawnSync('node', args, { encoding: 'utf8' });
 
-    it('should fail without correct sizes', function() {
+            assert.equal(proc.status, 1, 'Exitcode not 1!');
+            assert.equal(/Config invalid/.test(proc.stdout), true, proc.stdout);
+            assert.equal(test[1].test(proc.stdout), true, proc.stdout);
 
-        var args = [
-            normalize(__dirname + '/../../bin/argus-eyes.js'),
-            'configtest',
-            '--config=tests/fixtures/configtest/invalid-sizes.json'
-        ];
-
-        var proc = spawnSync('node', args, { encoding: 'utf8' });
-
-        assert.equal(proc.status, 1, 'Exitcode not 1!');
-        assert.equal(/Config invalid/.test(proc.stdout), true, proc.stdout);
-        assert.equal(/sizes/.test(proc.stdout), true, proc.stdout);
-
-    });
-
-    it('should fail without pages', function() {
-
-        var args = [
-            normalize(__dirname + '/../../bin/argus-eyes.js'),
-            'configtest',
-            '--config=tests/fixtures/configtest/missing-pages.json'
-        ];
-
-        var proc = spawnSync('node', args, { encoding: 'utf8' });
-
-        assert.equal(proc.status, 1, 'Exitcode not 1!');
-        assert.equal(/Config invalid/.test(proc.stdout), true, proc.stdout);
-        assert.equal(/pages/.test(proc.stdout), true, proc.stdout);
-
-    });
-
-    it('should fail without correct pages', function() {
-
-        var args = [
-            normalize(__dirname + '/../../bin/argus-eyes.js'),
-            'configtest',
-            '--config=tests/fixtures/configtest/invalid-pages.json'
-        ];
-
-        var proc = spawnSync('node', args, { encoding: 'utf8' });
-
-        assert.equal(proc.status, 1, 'Exitcode not 1!');
-        assert.equal(/Config invalid/.test(proc.stdout), true, proc.stdout);
-        assert.equal(/pages/.test(proc.stdout), true, proc.stdout);
-
-    });
-
-    it('should fail without components', function() {
-
-        var args = [
-            normalize(__dirname + '/../../bin/argus-eyes.js'),
-            'configtest',
-            '--config=tests/fixtures/configtest/missing-components.json'
-        ];
-
-        var proc = spawnSync('node', args, { encoding: 'utf8' });
-
-        assert.equal(proc.status, 1, 'Exitcode not 1!');
-        assert.equal(/Config invalid/.test(proc.stdout), true, proc.stdout);
-        assert.equal(/components/.test(proc.stdout), true, proc.stdout);
-
-    });
-
-    it('should fail without correct components', function() {
-
-        var args = [
-            normalize(__dirname + '/../../bin/argus-eyes.js'),
-            'configtest',
-            '--config=tests/fixtures/configtest/invalid-components.json'
-        ];
-
-        var proc = spawnSync('node', args, { encoding: 'utf8' });
-
-        assert.equal(proc.status, 1, 'Exitcode not 1!');
-        assert.equal(/Config invalid/.test(proc.stdout), true, proc.stdout);
-        assert.equal(/components/.test(proc.stdout), true, proc.stdout);
-
-    });
-
-    it('should fail with incorrect wait-for-script', function() {
-
-        var args = [
-            normalize(__dirname + '/../../bin/argus-eyes.js'),
-            'configtest',
-            '--config=tests/fixtures/configtest/invalid-wait-for-script.json'
-        ];
-
-        var proc = spawnSync('node', args, { encoding: 'utf8' });
-
-        assert.equal(proc.status, 1, 'Exitcode not 1!');
-        assert.equal(/Config invalid/.test(proc.stdout), true, proc.stdout);
-        assert.equal(/wait-for-script/.test(proc.stdout), true, proc.stdout);
-
-    });
-
-    it('should fail with incorrect wait-for-delay', function() {
-
-        var args = [
-            normalize(__dirname + '/../../bin/argus-eyes.js'),
-            'configtest',
-            '--config=tests/fixtures/configtest/invalid-wait-for-delay.json'
-        ];
-
-        var proc = spawnSync('node', args, { encoding: 'utf8' });
-
-        assert.equal(proc.status, 1, 'Exitcode not 1!');
-        assert.equal(/Config invalid/.test(proc.stdout), true, proc.stdout);
-        assert.equal(/wait-for-delay/.test(proc.stdout), true, proc.stdout);
-
-    });
-
-    it('should fail with removed finished-when', function() {
-
-        var args = [
-            normalize(__dirname + '/../../bin/argus-eyes.js'),
-            'configtest',
-            '--config=tests/fixtures/configtest/invalid-wait-for-script-finished-when.json'
-        ];
-
-        var proc = spawnSync('node', args, { encoding: 'utf8' });
-
-        assert.equal(proc.status, 1, 'Exitcode not 1!');
-        assert.equal(/Config invalid/.test(proc.stdout), true, proc.stdout);
-        assert.equal(/finished-when was renamed to wait-for-script/.test(proc.stdout), true, proc.stdout);
-
-    });
-
-    it('should fail with incorrect phantomjs-flags', function() {
-
-        var args = [
-            normalize(__dirname + '/../../bin/argus-eyes.js'),
-            'configtest',
-            '--config=tests/fixtures/configtest/invalid-phantomjs-flags.json'
-        ];
-
-        var proc = spawnSync('node', args, { encoding: 'utf8' });
-
-        assert.equal(proc.status, 1, 'Exitcode not 1!');
-        assert.equal(/Config invalid/.test(proc.stdout), true, proc.stdout);
-        assert.equal(/phantomjs-flags must be an array of strings/.test(proc.stdout), true, proc.stdout);
+        });
 
     });
 
